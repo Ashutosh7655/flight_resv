a242a6230d3031d8a84ffb0384bb11e2d8c25536
Ashutosh mishra <mahendramishraabhi@gmail.com>
2025-07-08 21:49:53 +0530

    Your message

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const { ServerConfig, Queue } = require('./config');
+
+const apiRoutes = require('./routes');
+
+const CRON = require('./utils/common/cron-jobs');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
+
+// proxy middleware
+app.use('/bookingService/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, async () => {
+    console.log(`successfully started the server on PORT : ${ServerConfig.PORT}`);
+    CRON();
+    await Queue.connectQueue();
+    console.log('queue connected');
+})
+//console.log(process.env)
